38040fdac4b3d8d978ee2c7d214b75ec72526e91
David Westerink <davidakachaos@gmail.com>
2017-05-12 09:59:18 +0200

    Let say how long we wait

--- pokemongo_bot/cell_workers/camp_fort.py
+++ pokemongo_bot/cell_workers/camp_fort.py
@@ -146,6 +146,7 @@ class CampFort(BaseTask):
             if self.cluster["lured"] == 0:
                 self.bot.camping_forts = False # Allow hunter to move
                 self.stay_until -= NO_LURED_TIME_MALUS
+                self.logger.info("Lures gone, waiting for forts to be lured again until %s".format(self.stay_until.strftime("%H:%M")))
 
             self.walker.step(speed=0)
         elif self.walker.step():
